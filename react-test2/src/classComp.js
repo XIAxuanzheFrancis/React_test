d0ed9ba3ab5080f7066a26eaeef5fa2bb6930112
xiaxuanzhe <3058634081@qq.com>
2023-12-27 18:33:38 +0100

    composant

---
+++ react-test2/src/classComp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ClassComp extends React.Component{
+    render(){
+        return(<div>This is my first class component</div>)
+    }
+}
+
+export default ClassComp;
---
+++ react-test2/src/index.js
@@ -0,0 +1,28 @@
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ClassComp from './classComp';
+
+// function Hello(){
+//   return (<div>This is my first function component</div>)
+// }
+
+const Hello = () => <div>This is my second function component</div>
+
+ReactDOM.render(<ClassComp />, document.getElementById('root'))
+
+
