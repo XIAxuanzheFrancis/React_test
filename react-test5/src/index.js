0e518d5b287fb9a381f427f9a15aa6e3a2fed8a9
xiaxuanzhe <3058634081@qq.com>
2023-12-28 11:24:14 +0100

    Uncontrolled components

---
+++ react-test5/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component{
+  constructor(){
+    super()
+    this.inputref=React.createRef()
+  }
+  render(){
+    return(
+      <div>
+        <input type='text' ref={this.inputref}></input>
+        <button onClick={()=>{console.log('the value of the text is:',this.inputref.current.value)}}>Get the value of the text box</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
